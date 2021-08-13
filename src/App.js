9fcc7b6d3b4a210e14b33f5277f749ab58f495a6
olawale-o <omoogunolawale@gmail.com>
2021-08-13 09:14:02 +0100

    Import Home.js in App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
-const App = () => <div className="App"><Header /></div>;
+const App = () => (
+  <div className="App">
+    <Header />
+    <Home />
+  </div>
+);
 
 export default App;
