4f1b2079c3f5d332659bf106d620bce76f721092
olawale-o <omoogunolawale@gmail.com>
2021-08-13 09:45:20 +0100

    Import Switch in App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -10,9 +10,11 @@ const App = () => (
   <Router>
     <div className="App">
       <Header />
-      <Route path="/" component={Home} exact />
-      <Route path="/calculator" component={Calculator} />
-      <Route path="/quote" component={Quote} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/calculator" component={Calculator} />
+        <Route path="/quote" component={Quote} />
+      </Switch>
     </div>
   </Router>
 );
