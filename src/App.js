7c35c0b8bd1c7e12459c3d4d184ce52b853d5a50
olawale-o <omoogunolawale@gmail.com>
2021-08-13 09:31:18 +0100

    Import Quote.js in App.js file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 const App = () => (
   <div className="App">
     <Header />
     <Home />
     <Calculator />
+    <Quote />
   </div>
 );
 
