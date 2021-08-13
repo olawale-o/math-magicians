715bebb6593358c550b5249afc49d0d39dd1951b
olawale-o <omoogunolawale@gmail.com>
2021-08-13 09:36:33 +0100

    Import Quote.css in Quote.js file

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Quote.css';
 
-const Quote = () => {
+const Quote = () => (
   <div className="Quote">
-    Quote
+    <p>
+      Mathematics is not about numbers, equations, computations or
+      algorithms: it is about understanding. -William Paul Thurston
+    </p>
   </div>
-
-};
+);
 
 export default Quote;
