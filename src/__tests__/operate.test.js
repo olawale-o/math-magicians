82268d2336c6cabd9137189191c000ee60d4994c
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-18 16:33:44 +0200

    Test Home

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,38 +1,36 @@
-import operate from '../logic/operate'
+import operate from "../logic/operate";
 
-describe('#operate', ()=>{
-  describe('Passing unknown operation that is not "+, -, X, ÷, %" ', ()=>{
-    it('throws an error ', ()=>{
-      expect(()=>{
-        operate('5', '10', '$');
-      }).toThrowError("Unknown operation '$'")
-    })
-  })
-  describe('Plus', ()=> {
-    test('Adding two numbers', ()=>{
-      expect(operate('5', '10', '+')).toBe('15')
-    })
-  })
-  describe('Minus', ()=> {
-    test('Substracting two numbers', ()=>{
-      expect(operate('5', '10', '-')).toBe('-5')
-    })
-  })
-  describe('Times', ()=> {
-    test('two numbers multiplication', ()=>{
-      expect(operate('5', '10', 'x')).toBe('50')
-    })
-  })
-  describe('Division', ()=> {
-    test('calculation remainder', ()=>{
-      expect(operate('5', '10', '÷')).toBe('0.5')
-    })
-  })
-  describe('modulo', ()=> {
-    test('calculation remainder', ()=>{
-      expect(operate('5', '10', '%')).toBe('5')
-    })
-  })
-
-
-})
+describe("#operate", () => {
+  describe('Passing unknown operation that is not "+, -, X, ÷, %" ', () => {
+    it("throws an error ", () => {
+      expect(() => {
+        operate("5", "10", "$");
+      }).toThrowError("Unknown operation '$'");
+    });
+  });
+  describe("Plus", () => {
+    test("Adding two numbers", () => {
+      expect(operate("5", "10", "+")).toBe("15");
+    });
+  });
+  describe("Minus", () => {
+    test("Substracting two numbers", () => {
+      expect(operate("5", "10", "-")).toBe("-5");
+    });
+  });
+  describe("Times", () => {
+    test("two numbers multiplication", () => {
+      expect(operate("5", "10", "x")).toBe("50");
+    });
+  });
+  describe("Division", () => {
+    test("calculation remainder", () => {
+      expect(operate("5", "10", "÷")).toBe("0.5");
+    });
+  });
+  describe("modulo", () => {
+    test("calculation remainder", () => {
+      expect(operate("5", "10", "%")).toBe("5");
+    });
+  });
+});
