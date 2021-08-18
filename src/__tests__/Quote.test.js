63d8b68c4822bf29e311ae910795962d74b877ad
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-18 17:59:02 +0200

    Fix ESlint error

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import Quote from '../components/Quote';
 
 describe('Home', () => {
